9d8fa6a6b272a7e2d5c3c26a76a4b8f2de4977b2
BaranovAleksei <baranov.sys@gmail.com>
2020-10-06 21:31:57 +0300

    ignat HW2 -ok + refactoring

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,38 +1,42 @@
 import React from "react";
 import Affair from "./Affair";
-import {AffairType} from "./HW2";
+import {AffairType, FilterType} from "./HW2";
+import s from './Affairs.module.css'
 
-type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+
+export type AffairsPropsType = { // need to fix any
+  data: Array<AffairType>
+  setFilter: ( priority: FilterType ) => void
+  deleteAffairCallback: ( id: number ) => void
 }
 
 function Affairs(props: AffairsPropsType) {
-    const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
-            affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
-        />
-    ))
+  const mappedAffairs = props.data.map((a: AffairType) => (
+    <Affair // should work
+      key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+      affair={ a }
+      deleteAffairCallback={ props.deleteAffairCallback  }
+    />
+  ))
 
-    const setAll = () => {}; // need to fix
-    const setHigh = () => {};
-    const setMiddle = () => {};
-    const setLow = () => {};
+  const setAll = () => {  props.setFilter('all') }; // need to fix
+  const setHigh = () => { props.setFilter('high') };
+  const setMiddle = () => { props.setFilter('middle') };
+  const setLow = () => { props.setFilter('low') };
 
-    return (
-        <div>
-
-            {mappedAffairs}
-
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
-        </div>
-    );
+  return (
+    <div className={s.overlayHW2}>
+      <div className={s.overlayAffairs}>
+        {mappedAffairs}
+      </div>
+      <div className={s.overlayButton}>
+        <button onClick={ setAll }><span>All</span></button>
+        <button onClick={ setHigh }><span>High</span></button>
+        <button onClick={ setMiddle }><span>Middle</span></button>
+        <button onClick={ setLow }><span>Low</span></button>
+      </div>
+    </div>
+  );
 }
 
 export default Affairs;
