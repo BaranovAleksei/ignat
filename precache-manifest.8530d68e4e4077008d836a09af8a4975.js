fc4f06590b11c5c3c19ec6a57bc40c945771105c
BaranovAleksei <baranov.sys@gmail.com>
2020-11-05 16:45:36 +0300

    Updates

---
+++ precache-manifest.8530d68e4e4077008d836a09af8a4975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c954c704f4f37a0baf7b63ef9dcbffc3",
+    "url": "/BaranovAleksei/ignat/index.html"
+  },
+  {
+    "revision": "a339a9233a06cbb617eb",
+    "url": "/BaranovAleksei/ignat/static/css/main.39e71ead.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/BaranovAleksei/ignat/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BaranovAleksei/ignat/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a339a9233a06cbb617eb",
+    "url": "/BaranovAleksei/ignat/static/js/main.9a6fa677.chunk.js"
+  },
+  {
+    "revision": "7b97e0ab67d9fe2a14e4",
+    "url": "/BaranovAleksei/ignat/static/js/runtime-main.32472923.js"
+  }
+]);
