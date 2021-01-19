2c521b9e904da95868c235d5297b7577c64ba2b2
alekseibaranov <baranov.sys@gmail.com>
2021-01-19 17:37:36 +0300

    test deploy to githubPage

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React from "react"
+import { NavLink } from "react-router-dom"
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.nav}>
+           <div className={ s.item }>
+             <NavLink to='/pre-junior' activeClassName = { s.activeLink }>PreJunior</NavLink>
+           </div>
+          <div className={ s.item }>
+            <NavLink to='/junior' activeClassName = { s.activeLink }>Junior</NavLink>
+          </div>
+          <div className={ s.item }>
+            <NavLink to='/junior-plus' activeClassName = { s.activeLink }>JuniorPlus</NavLink>
+          </div>
 
         </div>
     );
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import s from './Error404.module.css';
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.pageError}>
+            <div className={s.imgBackground}>
+              <img src="https://static8.depositphotos.com/1023102/1066/i/950/depositphotos_10662658-stock-photo-3d-error-404-crossword.jpg" alt="error404"/>
+            </div>
         </div>
     );
 }
